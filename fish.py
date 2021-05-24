a43154111f3a9a893f151601fc30417bf9206adb
Mahmoud-Magdy67 <84690699+Mahmoud-Magdy67@users.noreply.github.com>
2021-05-24 03:22:02 +0200

    Add files via upload

---
+++ fish.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[32]:
+
+
+import numpy as np
+
+
+# In[33]:
+
+
+A = np.array([ [1,1],[(1/3),-1] ])
+B = np.array([100,0])
+X = np.linalg.solve(A,B)
+print(X)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
